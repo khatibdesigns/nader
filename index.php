6b63ebb6cce1c81487313479296e93aa1891be7a
Khatib Designs LTD <nader@khatibdesigns.com>
2024-05-09 22:15:47 +0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  
+// Store the file name into variable
+$file = 'Nader_Khatib_Portfolio_compressed.pdf';
+$filename = 'Nader_Khatib_Portfolio_compressed.pdf';
+  
+// Header content type
+header('Content-type: application/pdf');
+header('Content-Disposition: inline; filename="' . $filename . '"');
+header('Content-Transfer-Encoding: binary');
+header('Accept-Ranges: bytes');
+  
+// Read the file
+@readfile($file);
+  
+?>
